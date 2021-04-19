065833ba5c6f9e9c2c9bc890edbd376148e8bc02
gourav2001k <mailatgourav2001@gmail.com>
2021-04-20 02:03:41 +0530

    Completed Start Game Screen

--- GuessApp/screens/StartGameScreen.js
+++ GuessApp/screens/StartGameScreen.js
@@ -1,36 +1,102 @@
-import React from "react";
-import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Alert,
+} from "react-native";
 
 import Card from "../components/Card";
+import Input from "../components/Input";
+import NumberContainer from "../components/NumberContainer";
 import Colors from "../constants/colors";
 
 const StartGameScreen = ({}) => {
-  const resetHandler = () => {};
-  const confirmHandler = () => {};
-  return (
-    <View style={styles.screen}>
-      <Text style={styles.title}>Start a New Game</Text>
-      <Card style={styles.inputContainer}>
-        <Text>Select a Number</Text>
-        <TextInput />
-        <View style={styles.buttonContainer}>
-          <View style={styles.button}>
-            <Button
-              title={"Reset"}
-              onPress={resetHandler}
-              color={Colors.accent}
-            />
-          </View>
-          <View style={styles.button}>
-            <Button
-              title={"Confirm"}
-              onPress={confirmHandler}
-              color={Colors.primary}
-            />
-          </View>
-        </View>
+  const [enteredValue, setEnteredValue] = useState("");
+  const [confirmed, setConfirmed] = useState(false);
+  const [selectedNum, setSelectedNum] = useState();
+
+  const numberInputHandler = (inputText) => {
+    setEnteredValue(inputText.replace(/[^0-9]/g, ""));
+  };
+  const resetHandler = () => {
+    setEnteredValue("");
+    setConfirmed(false);
+  };
+  const confirmHandler = () => {
+    const chosenNumber = parseInt(enteredValue);
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert(
+        "Invalid Number!",
+        "Number has to be a number between 1 and 99.",
+        [
+          {
+            text: "Okay",
+            style: "destructive",
+            onPress: resetHandler,
+          },
+        ]
+      );
+      return;
+    }
+    setConfirmed(true);
+    setSelectedNum(chosenNumber);
+    setEnteredValue("");
+    Keyboard.dismiss();
+  };
+  let confirmedOut;
+  if (confirmed) {
+    confirmedOut = (
+      <Card style={styles.summaryContainer}>
+        <Text>You Selected</Text>
+        <NumberContainer>{selectedNum}</NumberContainer>
+        <Button title={"START GAME"} />
       </Card>
-    </View>
+    );
+  }
+  return (
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+      }}
+    >
+      <View style={styles.screen}>
+        <Text style={styles.title}>Start a New Game</Text>
+        <Card style={styles.inputContainer}>
+          <Text>Select a Number</Text>
+          <Input
+            style={styles.input}
+            blurOnSubmit
+            autoCapitalize="none"
+            autoCorrect={false}
+            keyboardType="number-pad"
+            maxLength={2}
+            onChangeText={numberInputHandler}
+            value={enteredValue}
+          />
+          <View style={styles.buttonContainer}>
+            <View style={styles.button}>
+              <Button
+                title={"Reset"}
+                onPress={resetHandler}
+                color={Colors.accent}
+              />
+            </View>
+            <View style={styles.button}>
+              <Button
+                title={"Confirm"}
+                onPress={confirmHandler}
+                color={Colors.primary}
+              />
+            </View>
+          </View>
+        </Card>
+        {confirmedOut}
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -54,6 +120,15 @@ const styles = StyleSheet.create({
   },
   button: {
     width: "40%",
+  },
+  input: {
+    width: 50,
+    textAlign: "center",
+  },
+  summaryContainer: {
+    marginTop: 20,
+    width: 200,
+    alignItems: "center",
   },
 });
 
